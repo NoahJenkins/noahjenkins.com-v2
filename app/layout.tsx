dffbb9b3da8bf1fef221c8067456f0971fe2ad4f
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-07-28 18:38:55 -0500

    resolving errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
   },
 }
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes: (string | undefined | null)[]) => classes.filter(Boolean).join(' ')
 
 export default function RootLayout({
   children,
